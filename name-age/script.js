bf9965e0965ae73d4d26fedba41ae5119db77d4a
francescabarnes <francescabarnes@protonmail.com>
2023-06-26 12:17:13 +0200

    updating button files and name-age

--- name-age/script.js
+++ name-age/script.js
@@ -1,18 +1,31 @@
 /* Make a new page with a text input and a button. 
 When the button is clicked, a fetch query is sent to the [agify API](https://agify.io/) with the name entered in the input. 
 When the request is finished display the result in a new div on the page. 
-Keep the the past requests on the page by creating a new div each time you make an API call. */
+Keep the the past requests on the page by creating a new div each time you make an API call. 
+Also add a <select> field with a few countries in it, to narrow down the search to a specific country. 
+You'll have to look in agify documentation to know more about that.
+Store the previous results in a localStorage so you don't have to fetch them again!
+*/
 
 const input = document.querySelector("input");
 const button = document.querySelector("button");
+const select = document.querySelector("select");
 const div = document.querySelector("div");
 
 button.addEventListener("click", () => {
-  fetch(`https://api.agify.io/?name=${input.value}`)
+  fetch(`https://api.agify.io/?name=${input.value}&country_id=${select.value}`)
     .then((response) => response.json())
     .then((data) => {
       const newDiv = document.createElement("div");
-      newDiv.textContent = `${data.name} is ${data.age} years old`;
+      newDiv.textContent = `I predict that ${data.name} is ${data.age} years old from ${data.country_id}`;
       div.appendChild(newDiv);
     });
+});
+
+const previousResults = JSON.parse(localStorage.getItem("results")) || [];
+
+previousResults.forEach((result) => {
+  const newDiv = document.createElement("div");
+  newDiv.textContent = `I predict that ${result.name} is ${result.age} years old from ${result.country_id}`;
+  div.appendChild(newDiv);
 });
